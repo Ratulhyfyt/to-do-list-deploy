9e8a2740963b13ccc15b5ac50acc4fb0bd0fe35e
jovit4 <112522826+jovit4@users.noreply.github.com>
2025-10-23 23:17:34 +1100

    Update task.actions.ts

--- src/actions/task.actions.ts
+++ src/actions/task.actions.ts
@@ -4,5 +4,7 @@ import type { Task } from "@/types/task";
 export async function addTask(task:Task) {
   try{
     const response = await axios.post('/task/add', task);
+  } catch (error) {
+    console.error(error);
   }
 }
